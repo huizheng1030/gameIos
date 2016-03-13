5af511f7522a6181b6f7cadf85c6fc0e49d4cb89
huizheng1030 <rexxar1030@gmail.com>
2016-03-13 04:02:14 -0400

    quick summit

--- CHYFIos/www/js/app.js
+++ CHYFIos/www/js/app.js
@@ -1,4 +1,3 @@
-// Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -24,21 +23,26 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+  .state('login', {
+    url: '/login',
+    views: {
+      'login': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
   })
 
-  // Each tab has its own nav history stack:
+    .state('tab', {
+    url: '/tab',
+    views: {
+      'tab': {
+        templateUrl: 'templates/tabs.html'
+      }
+    }
+  })
+
 
   .state('tab.dash', {
     url: '/dash',
@@ -80,6 +84,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('login');
 
 });
--- CHYFIos/www/js/controllers.js
+++ CHYFIos/www/js/controllers.js
@@ -21,6 +21,37 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+.controller('loginCtrl', function($scope, $http, $state) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  $scope.loginData = {};
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+//     var  postData = {
+
+//             "password": $scope.password,
+//             "username": $scope.username
+//           };
+
+//   $http.post('http://login_URL', postData)
+//   .success(function(data) {
+//     alert("SUCCESS")
+//     $state.go('tab');
+//   })
+//   .error(function(data) {
+//     alert("ERROR");
+// }); }  
+    $state.go('tab');
+  };
+})
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
